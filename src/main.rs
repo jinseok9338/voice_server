151e3326a0501bee8218af8b885404c55dd51939
Jinseok Seo (Jason Jin) <jinseok9338@gmail.com>
2023-06-29 18:26:19 +0900

    little bit of chagne

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@ mod database;
 mod domains;
 mod schema;
 
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use database::postgres_pool::Db;
 use domains::cat::controllers::cat_controller;
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
-
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
         cfg.service(cat_controller::get_cats);
         cfg.service(cat_controller::create_cat);
         cfg.service(cat_controller::get_cat);
