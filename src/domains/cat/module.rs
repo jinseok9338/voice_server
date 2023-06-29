3e8cd24c18dba6ff2b3d893ffcc0f32556ac06fe
Jinseok Seo (Jason Jin) <jinseok9338@gmail.com>
2023-06-29 18:33:38 +0900

    little bit of chagne

--- src/domains/cat/module.rs
+++ src/domains/cat/module.rs
@@ -1,1 +1,15 @@
 // this is where we combine the cat domain and export it to the main.rs
+// src/routes/cat_routes.rs
+use crate::domains::cat::controllers::cat_controller;
+use actix_web::web::{self, ServiceConfig};
+
+pub fn cat_routes(cfg: &mut ServiceConfig) {
+    cfg.service(
+        web::scope("")
+            .service(cat_controller::get_cats)
+            .service(cat_controller::create_cat)
+            .service(cat_controller::get_cat)
+            .service(cat_controller::update_cat)
+            .service(cat_controller::delete_cat),
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,13 @@ mod schema;
 
 use actix_web::web::ServiceConfig;
 use database::postgres_pool::Db;
-use domains::cat::controllers::cat_controller;
+use domains::cat::module::cat_routes;
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(cat_controller::get_cats);
-        cfg.service(cat_controller::create_cat);
-        cfg.service(cat_controller::get_cat);
-        cfg.service(cat_controller::update_cat);
-        cfg.service(cat_controller::delete_cat);
+        cat_routes(cfg);
     };
 
     Ok(config.into())
